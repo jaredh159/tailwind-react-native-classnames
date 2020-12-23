9de873289732eee718322e708c2bd2fc4e7bfa4c
Jared Henderson <jaredh159@users.noreply.github.com>
2020-12-23 14:55:25 -0500

    safer (though less customizable) box-shadow support (#10)

--- src/types.ts
+++ src/types.ts
@@ -7,7 +7,9 @@ export type ClassInput =
   | { [k: string]: boolean | string | number };
 
 export type TwStyles = Record<string, Record<string, string | number>>;
-export type RnStyle = { [key: string]: string[] | string | number };
+export type RnStyle = {
+  [key: string]: string[] | string | number | { [key: string]: number };
+};
 
 export interface TailwindFn {
   (strings: TemplateStringsArray, ...values: (string | number)[]): RnStyle;
