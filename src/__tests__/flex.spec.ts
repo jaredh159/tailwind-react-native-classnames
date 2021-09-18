775b8b1b43146f23427e403f01540bde5d69efa0
Jared Henderson <jared@netrivet.com>
2021-09-18 11:53:49 -0400

    v2.0.0-alpha.12

---
+++ src/__tests__/flex.spec.ts
@@ -0,0 +1,56 @@
+import { describe, test, expect } from '@jest/globals';
+import { create } from '..';
+import { TwTheme } from '../tw-config';
+
+describe(`flex grow/shrink`, () => {
+  let tw = create();
+  beforeEach(() => (tw = create()));
+
+  const cases: Array<[string, Record<string, string | number>]> = [
+    [`flex-shrink-0`, { flexShrink: 0 }],
+    [`flex-shrink`, { flexShrink: 1 }],
+    [`flex-grow-0`, { flexGrow: 0 }],
+    [`flex-grow`, { flexGrow: 1 }],
+    [`flex-grow-77`, { flexGrow: 77 }],
+  ];
+
+  test.each(cases)(`tw\`%s\` -> %s`, (utility, expected) => {
+    expect(tw.style(utility)).toMatchObject(expected);
+  });
+});
+
+describe(`flex shorthand utilities`, () => {
+  const cases: Array<[string, Record<string, string | number>, TwTheme['flex'] | null]> =
+    [
+      [`flex-auto`, { flexGrow: 1, flexShrink: 1, flexBasis: `auto` }, null],
+      [`flex-initial`, { flexGrow: 0, flexShrink: 1, flexBasis: `auto` }, null],
+      [`flex-none`, { flexGrow: 0, flexShrink: 0, flexBasis: `auto` }, null],
+      [`flex-1`, { flexGrow: 1, flexShrink: 1, flexBasis: `0%` }, null],
+
+      // unsupported
+      [`flex-revert`, {}, null],
+      [`flex-unset`, {}, null],
+      [`flex-min-content`, {}, null],
+
+      // arbitrary
+      [`flex-33`, { flexGrow: 33, flexBasis: `0%` }, null],
+
+      // configged with two numeric values
+      [`flex-custom`, { flexGrow: 11, flexShrink: 22 }, { custom: `11 22` }],
+
+      // configged with number/width combo
+      [`flex-custom2`, { flexGrow: 3, flexBasis: 10 }, { custom2: `3 10px` }],
+
+      // configged with 3 values
+      [
+        `flex-custom3`,
+        { flexGrow: 5, flexShrink: 6, flexBasis: `10%` },
+        { custom3: `5 6 10%` },
+      ],
+    ];
+
+  test.each(cases)(`tw\`%s\` -> %s`, (utility, expected, config) => {
+    const tw = create(config ? { theme: { flex: config } } : {});
+    expect(tw.style(utility)).toMatchObject(expected);
+  });
+});
---
+++ src/resolve/flex.ts
@@ -0,0 +1,72 @@
+import { TwTheme } from '../tw-config';
+import { StyleIR } from '../types';
+import { complete, parseStyleVal } from '../helpers';
+
+export function flexGrowShrink(
+  type: 'Grow' | 'Shrink',
+  value: string,
+  config?: TwTheme['flexGrow'] | TwTheme['flexShrink'],
+): StyleIR | null {
+  value = value.replace(/^-/, ``);
+  const configKey = value === `` ? `DEFAULT` : value;
+  const numericValue = Number(config?.[configKey] ?? value);
+  if (!Number.isNaN(numericValue)) {
+    return complete({ [`flex${type}`]: numericValue });
+  }
+  return null;
+}
+
+export function flex(value: string, config?: TwTheme['flex']): StyleIR | null {
+  value = config?.[value] || value;
+  if ([`min-content`, `revert`, `unset`].includes(value)) {
+    // unsupported
+    return null;
+  }
+
+  // @see https://developer.mozilla.org/en-US/docs/Web/CSS/flex
+  // MDN: One value, unitless number: flex-grow flex-basis is then equal to 0.
+  if (value.match(/^\d+$/)) {
+    return complete({
+      flexGrow: Number(value),
+      flexBasis: `0%`,
+    });
+  }
+
+  // MDN: Two values (both integers): flex-grow | flex-basis
+  let match = value.match(/^(\d+)\s+(\d+)$/);
+  if (match) {
+    return complete({
+      flexGrow: Number(match[1]),
+      flexShrink: Number(match[2]),
+    });
+  }
+
+  // MDN: Two values: flex-grow | flex-basis
+  match = value.match(/^(\d+)\s+([^ ]+)$/);
+  if (match) {
+    const flexBasis = parseStyleVal(match[2] ?? ``);
+    if (!flexBasis) {
+      return null;
+    }
+    return complete({
+      flexGrow: Number(match[1]),
+      flexBasis,
+    });
+  }
+
+  // MDN: Three values: flex-grow | flex-shrink | flex-basis
+  match = value.match(/^(\d+)\s+(\d+)\s+(.+)$/);
+  if (match) {
+    const flexBasis = parseStyleVal(match[3] ?? ``);
+    if (!flexBasis) {
+      return null;
+    }
+    return complete({
+      flexGrow: Number(match[1]),
+      flexShrink: Number(match[2]),
+      flexBasis,
+    });
+  }
+
+  return null;
+}
