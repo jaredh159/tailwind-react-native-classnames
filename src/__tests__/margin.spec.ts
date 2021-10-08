e668d1e20d0dd933a97a1edecc8f7b862fc5c4c8
Jared Henderson <jared@netrivet.com>
2021-10-08 09:41:15 -0400

    support m{dir}-auto utilities

---
+++ src/__tests__/margin.spec.ts
@@ -0,0 +1,65 @@
+import { describe, test, expect } from '@jest/globals';
+import { create } from '../';
+
+describe(`margin`, () => {
+  let tw = create();
+  beforeEach(() => (tw = create()));
+
+  const cases: Array<[string, Record<string, string | number>]> = [
+    [
+      `m-auto`,
+      {
+        marginTop: `auto`,
+        marginBottom: `auto`,
+        marginLeft: `auto`,
+        marginRight: `auto`,
+      },
+    ],
+    [`mt-auto`, { marginTop: `auto` }],
+    [`mb-auto`, { marginBottom: `auto` }],
+    [`ml-auto`, { marginLeft: `auto` }],
+    [`mr-auto`, { marginRight: `auto` }],
+    [`mx-auto`, { marginRight: `auto`, marginLeft: `auto` }],
+    [`my-auto`, { marginTop: `auto`, marginBottom: `auto` }],
+    [`mt-px`, { marginTop: 1 }],
+    [`ml-[333px]`, { marginLeft: 333 }],
+    [`-ml-1`, { marginLeft: -4 }],
+  ];
+
+  test.each(cases)(`tw\`%s\` -> %s`, (utility, expected) => {
+    expect(tw.style(utility)).toMatchObject(expected);
+  });
+
+  test(`margin w/extended theme`, () => {
+    tw = create({
+      theme: {
+        extend: {
+          spacing: {
+            custom: `1000rem`,
+          },
+        },
+      },
+    });
+
+    expect(tw`m-custom`).toMatchObject({
+      marginTop: 16000,
+      marginBottom: 16000,
+      marginLeft: 16000,
+      marginRight: 16000,
+    });
+
+    expect(tw`m-1`).toMatchObject({
+      marginTop: 4,
+      marginBottom: 4,
+      marginLeft: 4,
+      marginRight: 4,
+    });
+
+    expect(tw`m-0.5`).toMatchObject({
+      marginTop: 2,
+      marginBottom: 2,
+      marginLeft: 2,
+      marginRight: 2,
+    });
+  });
+});
