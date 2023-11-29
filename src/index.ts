193a0372b51d9570315be7d8589f7aba4f1b1640
Jared Henderson <jaredh159@users.noreply.github.com>
2023-11-29 09:31:06 -0500

    fix non-exported style fn, documented in readme

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import { Platform } from 'react-native';
-import { TailwindFn, RnColorScheme } from './types';
-import { TwConfig } from './tw-config';
+import type { TailwindFn, RnColorScheme, ClassInput, Style } from './types';
+import type { TwConfig } from './tw-config';
 import plugin from './plugin';
 import rawCreate from './create';
 
 // Apply default config and inject RN Platform
 const create = (twConfig: TwConfig = {}): TailwindFn => rawCreate(twConfig, Platform.OS);
 
-export { create, plugin };
-export type { TailwindFn, TwConfig, RnColorScheme };
+export type { TailwindFn, TwConfig, RnColorScheme, ClassInput, Style };
 export { useDeviceContext, useAppColorScheme } from './hooks';
 
 const tailwind = create();
+const style = tailwind.style;
 
 export default tailwind;
+
+export { create, plugin, style };
