7bbd8ed51d839223372d8335f8532aee673f4e43
Jared Henderson <jaredh159@users.noreply.github.com>
2023-11-29 09:22:24 -0500

    v3.6.5-beta1

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Platform } from 'react-native';
-import type { TailwindFn, RnColorScheme } from './types';
+import type { TailwindFn, RnColorScheme, ClassInput, Style } from './types';
 import type { TwConfig } from './tw-config';
 import plugin from './plugin';
 import rawCreate from './create';
@@ -7,7 +7,7 @@ import rawCreate from './create';
 // Apply default config and inject RN Platform
 const create = (twConfig: TwConfig = {}): TailwindFn => rawCreate(twConfig, Platform.OS);
 
-export type { TailwindFn, TwConfig, RnColorScheme };
+export type { TailwindFn, TwConfig, RnColorScheme, ClassInput, Style };
 export { useDeviceContext, useAppColorScheme } from './hooks';
 
 const tailwind = create();
