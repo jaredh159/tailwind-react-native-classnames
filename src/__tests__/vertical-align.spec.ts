762f6d590e15be0b5aa198e622b982958eec91c5
menghan.yan <menghan_yan@foxmail.com>
2024-03-06 09:31:08 +0800

    format code

--- src/__tests__/vertical-align.spec.ts
+++ src/__tests__/vertical-align.spec.ts
@@ -5,13 +5,12 @@ describe(`vertical align`, () => {
   let tw = create();
   beforeEach(() => (tw = create()));
 
-  const cases: Array<[string, Record<string, string>]> =
-    [
-      [`align-auto`, { verticalAlign: `auto` }],
-      [`align-top`, { verticalAlign: `top` }],
-      [`align-bottom`,{ verticalAlign: `bottom` }],
-      [`align-middle`, { verticalAlign: `middle` }],
-    ];
+  const cases: Array<[string, Record<string, string>]> = [
+    [`align-auto`, { verticalAlign: `auto` }],
+    [`align-top`, { verticalAlign: `top` }],
+    [`align-bottom`, { verticalAlign: `bottom` }],
+    [`align-middle`, { verticalAlign: `middle` }],
+  ];
 
   test.each(cases)(`tw\`%s\` -> %s`, (utility, expected) => {
     expect(tw.style(utility)).toEqual(expected);
